f26a4f72372abcbeb3945f363f850cf5c2eed5df
Eric Van Hensbergen <ericvh@gmail.com>
2014-06-20 12:27:07 -0500

    remove old gem5.opt before trying to update it

--- build.bash
+++ build.bash
@@ -2,6 +2,7 @@
 # Build and Clean in one step to keep image manageable
 
 scons -j4 --ignore-style build/ARM/gem5.opt
+rm -f /usr/local/bin/gem5.opt
 mv build/ARM/gem5.opt /usr/local/bin
 rm -rf build
 mkdir -p build/ARM
