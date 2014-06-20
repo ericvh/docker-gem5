838c5dc16a4c59b60c71bde27911773907ea310b
Eric Van Hensbergen <ericvh@gmail.com>
2014-06-20 08:24:17 -0500

    Create build.bash

---
+++ build.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Build and Clean in one step to keep image manageable
+
+scons --ignore-style build/ARM/gem5.opt
+mv build/ARM/gem5.opt /usr/local/bin
+rm -rf build
+mkdir -p build/ARM
+ln -s /usr/local/bin/gem5.opt build/ARM/gem5.opt
